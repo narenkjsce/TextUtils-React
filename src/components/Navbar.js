27ac6f59da451893861e7024b90582558ddad044
Narendra Patel <narenkjsce@gmail.com>
2022-06-25 15:07:33 +0530

    router commit
    Just to deploy on github we have commented router as of now.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar(props) {
     // <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>      
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">{props.title}</Link>
+        <a className="navbar-brand" href="#">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -19,14 +19,15 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto ">
             <li className="nav-item">
-              <Link className="nav-link active"  to="/">Home</Link>
+            <a className="nav-link active"  href="#">Home</a>
+              {/* <Link className="nav-link active"  to="/">Home</Link> */}
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link className="nav-link" to="/about">{props.aboutText}</Link>
             </li> 
             <li className="nav-item">
               <Link className="nav-link" to="/contactus">{props.ContactText}</Link>
-            </li>            
+            </li>             */}
           </ul>
           {/* <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
